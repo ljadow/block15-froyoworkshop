f9cd15b1dbab3ad07bdd018205647dc73fbf8eed
Lindsay Jadow <lindsayjadow@lindsays-mbp.mynetworksettings.com>
2023-11-16 15:06:57 -0500

    completed the workshop with Trina

---
+++ froyo.js
@@ -0,0 +1,27 @@
+//take user input with prompt
+const userInput = prompt('Input your favorite froyo flavors separated by commas', 'vanilla,vanilla,vanilla,strawberry,coffee,coffee')
+
+//create array of inputs
+const inputString = (userInput.split(","));
+
+function countFlavors(obj){
+    const orders={};
+    for(i=0;i<inputString.length;i++){
+        let flavor=inputString[i];
+        if(orders[flavor]){
+            orders[flavor] +=1
+        } else {
+            orders[flavor]=1
+        }
+    }
+    return orders
+}
+console.log(countFlavors(inputString))
+
+/* output
+- the flavors must become the keys
+- the frequency must become the values 
+*/
+
+
+//Googled around and got help from here https://www.geeksforgeeks.org/count-frequency-of-an-array-item-in-javascript/
